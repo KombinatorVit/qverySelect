c3fba373c7824cc93bcf772584e90e65ed7caf82
Kombinator <slonik0775@gmail.com>
2022-03-29 13:29:47 +0300

    fetch - practica

--- js/test.js
+++ js/test.js
@@ -1,52 +1,52 @@
-// "use strict";
+// // "use strict";
 
-// console.log("Запрос данных...");
+// // console.log("Запрос данных...");
 
-// const req = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     console.log("Подготовка данных...");
+// // const req = new Promise((resolve, reject) => {
+// //   setTimeout(() => {
+// //     console.log("Подготовка данных...");
 
-//     const product = {
-//       name: "TV",
-//       price: 2000,
-//     };
+// //     const product = {
+// //       name: "TV",
+// //       price: 2000,
+// //     };
 
-//     resolve(product);
-//   }, 2000);
-// });
+// //     resolve(product);
+// //   }, 2000);
+// // });
 
-// req
-//   .then((product) => {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         product.status = "order";
-//           resolve(product);
-//       }, 2000);
-//     });
-//   })
-//   .then((data) => {
-//     data.modify = true;
-//     return data;
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   }).catch(() => {
-// console.error('Произошла оишбка'); 
-//   }).finally(() => {
-//       console.log('Finally')
-//   })
+// // req
+// //   .then((product) => {
+// //     return new Promise((resolve, reject) => {
+// //       setTimeout(() => {
+// //         product.status = "order";
+// //           resolve(product);
+// //       }, 2000);
+// //     });
+// //   })
+// //   .then((data) => {
+// //     data.modify = true;
+// //     return data;
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //   }).catch(() => {
+// // console.error('Произошла оишбка'); 
+// //   }).finally(() => {
+// //       console.log('Finally')
+// //   })
  
 
 
-const test = time => {
-    return new Promise(resolve =>{
-        setTimeout(() => resolve(), time);
-    });
-};
+// const test = time => {
+//     return new Promise(resolve =>{
+//         setTimeout(() => resolve(), time);
+//     });
+// };
 
-// test(1000).then(() => console.log('1000 ms'));
-// test(2000).then(() => console.log('2000 ms'));
+// // test(1000).then(() => console.log('1000 ms'));
+// // test(2000).then(() => console.log('2000 ms'));
 
-Promise.race([test(1000),test(2000) ]).then(() => {
-console.log('ALL')
-});
+// Promise.race([test(1000),test(2000) ]).then(() => {
+// console.log('ALL')
+// }); 
