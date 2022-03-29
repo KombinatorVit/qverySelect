6766693fc5a6b869a832a2e78075c1ffe0d03d3b
Kombinator <slonik0775@gmail.com>
2022-03-29 14:14:47 +0300

    Array method

--- js/test.js
+++ js/test.js
@@ -1,52 +1,49 @@
-// // "use strict";
-
-// // console.log("Запрос данных...");
-
-// // const req = new Promise((resolve, reject) => {
-// //   setTimeout(() => {
-// //     console.log("Подготовка данных...");
-
-// //     const product = {
-// //       name: "TV",
-// //       price: 2000,
-// //     };
-
-// //     resolve(product);
-// //   }, 2000);
-// // });
-
-// // req
-// //   .then((product) => {
-// //     return new Promise((resolve, reject) => {
-// //       setTimeout(() => {
-// //         product.status = "order";
-// //           resolve(product);
-// //       }, 2000);
-// //     });
-// //   })
-// //   .then((data) => {
-// //     data.modify = true;
-// //     return data;
-// //   })
-// //   .then((data) => {
-// //     console.log(data);
-// //   }).catch(() => {
-// // console.error('Произошла оишбка'); 
-// //   }).finally(() => {
-// //       console.log('Finally')
-// //   })
- 
+ // filter 
 
 
-// const test = time => {
-//     return new Promise(resolve =>{
-//         setTimeout(() => resolve(), time);
-//     });
-// };
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart' ];
 
-// // test(1000).then(() => console.log('1000 ms'));
-// // test(2000).then(() => console.log('2000 ms'));
+// const shortNames = names.filter((name) => {
+// return name.length<5;
+// });
 
-// Promise.race([test(1000),test(2000) ]).then(() => {
-// console.log('ALL')
-// }); 
+// console.log(shortNames)
+
+
+//map
+
+// let answers = ['IvAn', 'AnnA', 'Hello'];
+
+// const result  = answers.map(item => item.toLowerCase() );
+// console.log(result);  
+
+
+// every/some 
+
+// const some = [4, 'qwq', 'dsfsvg'];
+
+// // console.log(some.some(item => typeof(item) === 'number' ));
+// console.log(some.every(item => typeof(item) === 'number' ));
+
+
+//reduce  
+
+
+// const arr = [4, 5, 1, 3, 2, 6];
+// const result = arr.reduce((sum, current) => sum + current);
+
+// const arr = ['dsd', 'dsd', 'dsdsd'];
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone');
+console.log(newArr);
